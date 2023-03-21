329226ae1ca713d6f1dfd2179c50db961b7ea2a9
Jordy van der Pol <jordyvdpol@gmail.com>
2023-03-21 15:43:28 +0100

    ingredient class (#8)

--- index.php
+++ index.php
@@ -4,22 +4,27 @@ require_once("lib/database.php");
 require_once("lib/artikel.php");
 require_once("lib/user.php");
 require_once("lib/keuken_type.php"); 
+require_once("lib/ingredient.php");
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
 $user = new user($db -> getConnection());
 $keukenType = new keukenType($db->getConnection());
+$ingredient = new ingredient($db->getConnection());
 
 
 /// VERWERK 
 $dataArtikel = $art->selecteerArtikel(3);
-$dataUser = $user -> selecteerUser(2);
-$dataKeukenType = $keukenType -> selecteerKeukenType(1);
+// $dataUser = $user -> selecteerUser(2);
+// $dataKeukenType = $keukenType -> selecteerKeukenType(1);
+$dataIngredient = $ingredient -> selecteerIngredient(1);
 
 /// RETURN
-var_dump($dataArtikel);
-echo '<br>';	
-var_dump($dataUser);
-echo "<br>";
-var_dump($dataKeukenType);
+// var_dump($dataArtikel);
+// echo '<br>';	
+// var_dump($dataUser);
+// echo "<br>";
+// var_dump($dataKeukenType);
+echo "<pre>";
+var_dump($dataIngredient);
---
+++ lib/ingredient.php
@@ -0,0 +1,47 @@
+<?php
+
+class ingredient{
+    
+    private $art;
+    private $connection;
+
+    public function __construct($connection) {
+        $this -> connection = $connection;
+        $this -> art = new artikel($connection);
+    }
+
+    private function kiesArtikel($artikel_id){
+        $artikelData = $this-> art-> selecteerArtikel($artikel_id);
+        return ($artikelData);
+    }
+
+    public function selecteerIngredient($gerecht_id) {
+        $ingredienten = [];
+        $sql = "SELECT * FROM ingredient WHERE gerecht_id = $gerecht_id";
+
+        $result = mysqli_query($this->connection, $sql);
+        // $ingredient = mysqli_fetch_array($result, MYSQLI_ASSOC); 
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { // mysql_fetch_array() returns false when no more rows are available --> no looping increment needed
+            var_dump($row);
+            echo "<br>";
+            $artikel_id = $row["artikel_id"];
+            $artikel =  $this -> kiesArtikel($artikel_id);
+
+            $ingredienten[] = [
+                "id" => $row["id"],
+                "gerecht_id" => $row["gerecht_id"],
+                "artikel_id" => $row["artikel_id"],
+                "aantal" => $row["aantal"],
+                "naam" => $artikel["naam"],
+                "omschrijving" => $artikel["omschrijving"],
+                "prijs" => $artikel["prijs"],
+                "eenheid" => $artikel["eenheid"],
+                "verpakking" => $artikel ["verpakking"]
+            ];
+            
+
+        }
+        return ($ingredienten);
+    }
+}
